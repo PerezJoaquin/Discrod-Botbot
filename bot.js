3855749ad4f3231754d84dd4537ffda44f2cc95f
Joaquin Perez <joaquin.omar96@gmail.com>
2018-04-20 19:08:34 -0300

    Update and rename botT.js to bot.js

--- bot.js
+++ bot.js
@@ -1,21 +1,21 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const bot = new Discord.Client();
 
-client.on('ready', () => {
+bot.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
+bot.on('message', message => {
     if (message.content === 'ping') {
     	message.channel.send('PONG!');
   	}
 });
 
-client.on('message', message => {
+bot.on('message', message => {
     if (message.content === 'bing') {
     	message.reply('BONG!');
   	}
 });
 
 // THIS  MUST  BE  THIS  WAY
-client.login(process.env.BOT_TOKEN);
+bot.login(process.env.BOT_TOKEN);
