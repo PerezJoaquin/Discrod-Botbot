57bc131dddb7694340fb285c55b3c00aeb110cb8
Joaquin Perez <joaquin.omar96@gmail.com>
2018-04-20 18:50:08 -0300

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -1,22 +1,21 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+var Discord = require('discord.js');
+//var logger = require('winston');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 // Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
+//logger.remove(logger.transports.Console);
+/*logger.add(logger.transports.Console, {
     colorize: true
 });
-logger.level = 'debug';
+logger.level = 'debug';*/
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: process.env.BOT_TOKEN,
    autorun: true
 });
 bot.on('ready', function (evt) {
-    logger.info('Connected');
+    /*logger.info('Connected');
     logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info(bot.username + ' - (' + bot.id + ')');*/
 });
 bot.on('message', function (user, userID, channelID, message, evt) {
     try{
