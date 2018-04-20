4870d1a3dd8e223316a53014dd94fe145bacce2c
Joaquin Perez <joaquin.omar96@gmail.com>
2018-04-20 19:12:40 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ bot.on('ready', () => {
     console.log('I am ready!');
 });
 
-bot.on('message', message => {
+/*bot.on('message', message => {
     if (message.content === 'ping') {
     	message.channel.send('PONG!');
   	}
@@ -15,6 +15,15 @@ bot.on('message', message => {
     if (message.content === 'bing') {
     	message.reply('BONG!');
   	}
+});*/
+
+bot.on('message', function (user, userID, channelID, message, evt){
+    if(message.toLowerCase() == "<@410710444182077450>"){
+            bot.sendMessage({
+                to: channelID,
+                message: ':eggplant:'
+            });
+}
 });
 
 // THIS  MUST  BE  THIS  WAY
