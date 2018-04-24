178664b8f23951a3ea2f9d3d780c688ade5c1dca
Joaquin Perez <joaquin.omar96@gmail.com>
2018-04-24 04:33:56 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ bot.on('ready', () => {
 bot.on('message', message => {
     if (message.content === 'ping') {
     	//message.channel.send('PONG!');
-        request('http://www.google.com', function (error, response, body) {
+        XMLHttpRequest('http://www.google.com', function (error, response, body) {
           console.log('error:', error); // Print the error if one occurred
           console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
           console.log('body:', body); // Print the HTML for the Google homepage.
