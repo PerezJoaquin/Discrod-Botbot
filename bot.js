bfaefd25ca5f187c234183a7e8e28e48ce0a0517
Joaquin Perez <joaquin.omar96@gmail.com>
2018-04-24 04:48:53 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -25,11 +25,6 @@ bot.on('message', message => {
   	}
 });
 
-request('http://www.google.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
 //tag
 bot.on('message', message => {
     if(message.content.toLowerCase() == "<@410710444182077450>"){
