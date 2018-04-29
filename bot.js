6a831c6992243d0e6213b54b47f552b3a5c43642
Joaquin Perez <joaquin.omar96@gmail.com>
2018-04-28 21:30:36 -0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-//const XMLHttpRequest = require('request');
-//const XMLHttpRequest = require('request-promise')
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 bot.on('ready', () => {
@@ -19,20 +17,9 @@ bot.on('ready', () => {
 bot.on('message', message => {
     if (message.content === 'ping') {
     	message.channel.send('PONG! HTTP v5');
-        //XMLHttpRequest('https://danbooru.donmai.us/posts/random.json', function (error, response, body) {
-            //console.log('body:', body); // Print the HTML for the Google homepage.
-            //return body;
-            //response = JSON.parse(body);
-        var psor = " ";
-        /*httpGet("https://danbooru.donmai.us/posts/random.json", function(repose){
-            psor = repose;
-            console.log("ping: " + psor);
-        });*/
-        d = new Date();
-        console.log("pingg:" + d + "\n");
+        /*d = new Date();
+        console.log("pingg:" + d + "\n");*/
         console.log("pong: " + httpGet("https://danbooru.donmai.us/posts/random.json"));
-        
-        //});
   	}
 });
 
@@ -173,39 +160,136 @@ bot.on('message', message => {
                 console.log("sal db");
             }
         }
+  //SAFEBOORU------------------------------------------------------
+        if(args[0]+" "+args[1] == "!bb sb"){
+            if(args[2] != null){
+                if(args[3] != null){
+                    countSel = [];
+                    var d = new Date();
+                    minsec = (d.getMinutes()*60) + d.getSeconds();
+                    for(i = 0; i < args[3]; i++){                       
+                        response = JSON.parse(httpGet("https://safebooru.donmai.us/posts/random.json?tags="+args[2]));  
+                        d = new Date();
+                        minsec = (d.getDay()*24*60*60) + (d.getHours()*60*60) + (d.getMinutes()*60) + d.getSeconds();           
+                        minsec2 = 0;
+                        if(i != 0){
+                            while(!comprobar(countSel, response["id"])){
+                                response2 = JSON.parse(httpGet("https://safebooru.donmai.us/posts/random.json?tags="+args[2]));
+                                response = response2;
+                                daux = new Date();
+                                minsec2 = (daux.getDay()*24*60*60) + (daux.getHours()*60*60) + (daux.getMinutes()*60) + daux.getSeconds();
+                                console.log("minsec: " +minsec+"\nminsec2:"+minsec2);
+                                if(minsec2 - minsec > 15){
+                                    console.log("break");
+                                    break;
+                                }
+                            }
+                        }
+                        if(minsec2 - minsec > 15){
+                            console.log("minsec2: " +minsec2)
+                            console.log("break");
+                            break;
+                        }
+                        countSel[i] = response["id"];
+                        selimg = response;
+                        if(selimg["tag_string_artist"] == ''){
+                            selimg["tag_string_artist"] = "Desconocido";
+                        }
+                        if(selimg["file_url"][0] == "/"){
+                            linklink = "http://hijiribe.donmai.us"+selimg["file_url"];
+                        }else{
+                            linklink = selimg["file_url"]
+                        }
+                        var lastFive = selimg["file_url"].substr(selimg["file_url"].length - 3);
+                        if(this.lastFive == "ebm"){
+                            message.channel.send({embed: {
+                                    color: 0xf17c41,
+                                    description: '**Artista**: `' +selimg["tag_string_artist"]+"`\n**[Fuente]("+selimg['source']+")** \n**[Link]("+linklink+")**\n**[Safebooru](http://danbooru.donmai.us/posts/"+selimg["id"]+")**",
+                                    video: {url: linklink}
+                                }
+                           });
+                        }else{
+                            message.channel.send({embed: {
+                                    color:0xf17c41, 
+                                    description: '**Artista**: `' +selimg["tag_string_artist"]+"`\n**[Fuente]("+selimg['source']+")** \n**[Link]("+linklink+")**\n**[Safebooru](http://danbooru.donmai.us/posts/"+selimg["id"]+")**",
+                                    image: {url: linklink}
+                                }
+                            });
+                        }
+                        console.log("sal sb tag num");
+                    }
+                }else{
+                    response = JSON.parse(httpGet("https://safebooru.donmai.us/posts/random.json?tags="+args[2]));
+                    selimg = response;
+                    if(selimg["tag_string_artist"] == ''){
+                        selimg["tag_string_artist"] = "Desconocido";
+                    }
+                    if(selimg["file_url"][0] == "/"){
+                        linklink = "http://hijiribe.donmai.us"+selimg["file_url"]
+                    }else{
+                        linklink = selimg["file_url"]
+                    }
+                    var lastFive = selimg["file_url"].substr(selimg["file_url"].length - 3); // => "Tabs1"
+                    if(this.lastFive == "ebm"){
+                        message.channel.send({embed: {
+                                color:0xf17c41, 
+                                description: '**Artista**: `' +selimg["tag_string_artist"]+"`\n**[Fuente]("+selimg['source']+")** \n**[Link]("+linklink+")**\n**[Safebooru](http://danbooru.donmai.us/posts/"+selimg["id"]+")**",
+                                video: {url: linklink}
+                            }
+                        });
+                    }else{
+                        message.channel.send({embed: {
+                                color:0xf17c41, 
+                                description: '**Artista**: `' +selimg["tag_string_artist"]+"`\n**[Fuente]("+selimg['source']+")** \n**[Link]("+linklink+")**\n**[Safebooru](http://danbooru.donmai.us/posts/"+selimg["id"]+")**",
+                                image: {url: linklink}
+                            }
+                        });
+                    }
+                    console.log("sale sb tag");
+                }
+            }else{
+                /*XMLHttpRequest('http://danbooru.donmai.us/posts/random.json', function (error, response, body) {
+                    response = body;
+                });*/
+                pstoa = httpGet("https://safebooru.donmai.us/posts/random.json");
+                response = JSON.parse(pstoa);
+                selimg = response;
+                if(selimg["tag_string_artist"] == ''){
+                    selimg["tag_string_artist"] = "Desconocido";
+                }
+                if(selimg["file_url"][0] == "/"){
+                    linklink = "http://hijiribe.donmai.us"+selimg["file_url"]
+                }else{
+                    linklink = selimg["file_url"]
+                }
+                var lastFive = selimg["file_url"].substr(selimg["file_url"].length - 3);
+                if(this.lastFive == "ebm"){
+                    message.channel.send({embed: {
+                            color:0xf17c41, 
+                            description: '**Artista**: `' +selimg["tag_string_artist"]+"`\n**[Fuente]("+selimg['source']+")** \n**[Link]("+linklink+")**\n**[Safebooru](http://danbooru.donmai.us/posts/"+selimg["id"]+")**",
+                            video: {url: linklink}
+                        }
+                    });
+                }else{
+                    message.channel.send({embed: {
+                            color:0xf17c41, 
+                            description: '**Artista**: `' +selimg["tag_string_artist"]+"`\n**[Fuente]("+selimg['source']+")** \n**[Link]("+linklink+")**\n**[Safebooru](http://danbooru.donmai.us/posts/"+selimg["id"]+")**",
+                            image: {url: linklink}
+                        }
+                    });
+                }
+                console.log("sal sb");
+            }
+        }
+        
     }catch(e){
         console.log(e);
         message.channel.send(e);
         message.channel.send('Ocurrió un error con el pedido');
     }
 });
-function httpGet(theUrl/*, callback*/){
-    /*var repose = " ";
-    d = new Date();
-    console.log("function op: " + d + "\n");
-    minsec = (d.getDay()*24*60*60) + (d.getHours()*60*60) + (d.getMinutes()*60) + d.getSeconds();
-    minsec2 = 0;
-    XMLHttpRequest(theUrl, function (error, response, body) {
-        d = new Date();
-        console.log("ini req: " + d + "\n");
-        console.log("body: " + body + "\n");
-        repose = body;
-        console.log("repose: " + repose + "\n");
-        d = new Date();
-        console.log("end req: " + d + "\n");
-    });
-    while(repose[0] != "{"){
-        daux = new Date();
-        minsec2 = (daux.getDay()*24*60*60) + (daux.getHours()*60*60) + (daux.getMinutes()*60) + daux.getSeconds();
-        console.log("minsec: " +minsec+"\nminsec2:"+minsec2);
-        if(minsec2 - minsec > 15){
-            console.log("break");
-            break;
-        }
-    }
-    return repose;*/
-    //callback(repose);
-    
+
+function httpGet(theUrl){
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
     //test xmlHttp.open( "GET", "https://danbooru.donmai.us/posts/2113025.json", false );
@@ -216,6 +300,7 @@ function httpGet(theUrl/*, callback*/){
         return httpGet(theUrl);
     }
 }
+
 function comprobar(arr, num){
     try{
         for(j=0;j<arr.length; j++){
